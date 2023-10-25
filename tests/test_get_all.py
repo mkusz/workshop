873c3921c65b0c7a6485399e79116405d2f8fa9c
Maciej Kusz <9740637+mkusz@users.noreply.github.com>
2023-10-25 17:13:46 +0200

    Test get all

--- tests/test_get_all.py
+++ tests/test_get_all.py
@@ -2,10 +2,11 @@ import pytest
 import requests
 
 
-# @pytest.mark.smoke
+@pytest.mark.smoke
+@pytest.mark.wip
 def test_get_all(admin_cookie, env_settings):
     response = requests.get(
         url=f"{env_settings.base_url}booking",
         cookies=admin_cookie,
     )
-    print(response.content)
+    assert len(response.json()) >= 1
